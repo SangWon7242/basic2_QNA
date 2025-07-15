71f6c1f9e71f45a758c1228e4721285822f6e62c
SangWon7242 <gotkdqja@gmail.com>
2025-07-15 17:39:55 +0900

    question field ManyToOne

--- src/main/java/com/sbs/basic2/boundedContext/answer/Entity/Answer.java
+++ src/main/java/com/sbs/basic2/boundedContext/answer/Entity/Answer.java
@@ -1,5 +1,6 @@
 package com.sbs.basic2.boundedContext.answer.Entity;
 
+import com.sbs.basic2.boundedContext.question.entity.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,5 +20,6 @@ public class Answer {
 
   private LocalDateTime createDate;
 
-//  private Question question;
+  @ManyToOne // Manu-to-One 관계 설정 : 여러 개의 답변이 하나의 질문에 속할 수 있음
+  private Question question; // foreign key로 Question 엔티티를 참조
 }
