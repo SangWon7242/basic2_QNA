fcd8e8f0f1207ca208bcdb0d1a9a8ea3e5466195
SangWon7242 <gotkdqja@gmail.com>
2025-07-17 11:37:13 +0900

    @OneToMany filed

--- src/main/java/com/sbs/basic2/boundedContext/question/entity/Question.java
+++ src/main/java/com/sbs/basic2/boundedContext/question/entity/Question.java
@@ -1,10 +1,12 @@
 package com.sbs.basic2.boundedContext.question.entity;
 
+import com.sbs.basic2.boundedContext.answer.Entity.Answer;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,4 +23,11 @@ public class Question {
   private String content;
 
   private LocalDateTime createDate; // 생성 날짜 및 시간
+
+  // One-to-Many 관계 설정 : 하나의 질문에 여러 개의 답변이 있을 수 있음
+  // mappedBy = "question"은 Answer 엔티티의 question 필드에 의해 매핑됨
+  // cascade = CascadeType.REMOVE : 질문이 삭제될 때 관련된 답변도 함께 삭제됨
+  // 하는 이유 : 데이터베이스는 리스트, 배열 등의 복잡한 자료구조를 지원하지 않기 때문에
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
 }
